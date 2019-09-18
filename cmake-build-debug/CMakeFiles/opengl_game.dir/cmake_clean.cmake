4fa2e1428786f11abead12acd22cfa88847407bd
Link <sandlincoln@gmail.com>
2019-09-18 10:40:55 -0700

    set up cmake with opengl

---
+++ cmake-build-debug/CMakeFiles/opengl_game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl_game.dir/source/main.cpp.obj"
+  "opengl_game.pdb"
+  "opengl_game.exe"
+  "opengl_game.exe.manifest"
+  "libopengl_game.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
