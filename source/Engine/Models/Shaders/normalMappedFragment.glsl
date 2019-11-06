3c8fe7dcd9c32b85f1e630bccd03f43235a66f56
Dexter <dexterwest317@gmail.com>
2019-11-06 11:18:18 -0800

    Normal and specular maps fully functional

--- source/Engine/Models/Shaders/normalMappedFragment.glsl
+++ source/Engine/Models/Shaders/normalMappedFragment.glsl
@@ -11,6 +11,8 @@ uniform sampler2D texture0;
 uniform sampler2D texture1;
 //normal map
 uniform sampler2D texture2;
+//specular map
+uniform sampler2D texture3;
 
 uniform vec3 lightPos;
 uniform vec3 lightColor;
@@ -22,7 +24,7 @@ void main()
 
     // obtain normal from normal map in range [0,1]
     vec3 normal = texture(texture2, texCoord).rgb;
-    // transform normal vector to range [-1,1]
+    // transform normal vector to range [-1,1]  
     normal = normalize(normal * 2.0 - 1.0);
     normal = normalize(TBN * normal);
 
@@ -33,7 +35,8 @@ void main()
     float ambientStrength = .5;
     vec3 ambient = ambientStrength * lightColor;
 
-    float specularStrength = 0.3;
+    //value is a scaling factor for the specular map
+    float specularStrength = texture(texture3, texCoord).r * 4;
     vec3 viewDir = normalize(viewPos - fragPos);
     vec3 reflectDir = reflect(-lightDir, normal);
     float spec = pow(max(dot(viewDir, reflectDir), 0), 5);
